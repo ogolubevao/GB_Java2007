d3294f44c338205b957400620bdbb9973cb1af7d
oolubevao <olga602god@gmail.com>
2021-12-23 19:06:00 +0300

    Проба 3 версия

--- src/main/java/lesson_1_1/FirstApp.java
+++ src/main/java/lesson_1_1/FirstApp.java
@@ -3,6 +3,8 @@ package lesson_1_1;
 public class FirstApp {
     public static void main(String[] args) {
         System.out.println("hello, Olga!");
+
+
         System.out.println("version3");
     }
 }
