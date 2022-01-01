13fa7486cf5010a83eaabd72a027a7f19f04f49d
oolubevao <olga602god@gmail.com>
2022-01-01 16:11:06 +0300

    Домашка урока 3, задания 6-8

--- src/main/java/lesson_1_3/HomeWorkApp_1_3.java
+++ src/main/java/lesson_1_3/HomeWorkApp_1_3.java
@@ -57,7 +57,37 @@ public class HomeWorkApp_1_3 {
         System.out.println(Arrays.toString(returnArray(5, 9)));
         System.out.println();
 
+        //Задание №6
+        int[] array6 = {100, 5, 3, 2, 11, 4, 5, -89, 4, 8, 9, 1};
+        //int minValue = array6[0];
+        int minValue = Integer.MAX_VALUE;
+        //int maxValue = array6[0];
+        int maxValue = Integer.MIN_VALUE;
+        for (int i = 0; i < array6.length; i++) {
+            if (array6[i] < minValue) {
+                minValue = array6[i];
+            }
+            if (array6[i] > maxValue) {
+                maxValue = array6[i];
+            }
+        }
+        System.out.println("#6");
+        System.out.println("minValue = " + minValue + "  maxValue = " + maxValue);
+        System.out.println();
 
+        //Проверка задания №7
+        System.out.println("#7");
+        int[] array7 = {1, 5, 3, 3};
+        System.out.println(Arrays.toString(array7));
+        System.out.println(isHaveEqualParts(array7));
+        System.out.println();
+
+        //Проверка задания №8
+        System.out.println("#8");
+        int[] array8 = {1, 5, 47, 1, -98, 0, -7};
+        System.out.println(Arrays.toString(array8));
+        System.out.println(Arrays.toString(getShiftedArray(array8,1)));
+        System.out.println();
     }
 
     //Реализация задания №5
@@ -69,5 +99,61 @@ public class HomeWorkApp_1_3 {
         return arr;
     }
 
+    //Реализация задания №7
+    public static boolean isHaveEqualParts(int[] arr) {
+        int sumLeft;
+        int sumRight;
+        int count = 0;
 
+
+        //Проверка на пустой массив
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                count++;
+            }
+        }
+        if (count == arr.length) {
+            System.out.println("Массив должен быть не пустой");
+            return false;
+        }
+
+        //Нахождение сумм правой и левой части массива и их сравнение
+        for (int i = 0; i < arr.length - 1; i++) {
+            sumLeft = 0;
+            sumRight = 0;
+            for (int iLeft = i; iLeft >= 0; iLeft--) {
+                sumLeft += arr[iLeft];
+            }
+            for (int iRight = i + 1; iRight < arr.length; iRight++) {
+                sumRight += arr[iRight];
+            }
+            if (sumLeft == sumRight) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Реализация задания №8
+    public static int[] getShiftedArray(int[] arr, int n) {
+
+        //Проверка на массив из 1 значения. Его сдвигать не надо
+        if (arr.length == 1) {
+            return arr;
+        }
+
+        //Смещение массива на 1 символ через буферную int переменную,
+        // повтор смещения через рекурсию, если n > 0
+        if (n > 0) {
+            int buffer = arr[0];
+            arr[0] = arr[arr.length - 1];
+            for (int i = arr.length - 2; i >= 1; i--) {
+                arr[i + 1] = arr[i];
+            }
+            arr[1] = buffer;
+            return getShiftedArray(arr, n - 1);
+        } else {
+            return arr;
+        }
+    }
 }
