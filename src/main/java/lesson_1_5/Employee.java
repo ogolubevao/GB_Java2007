db48acec298cea1655e01b15a64d17f562a758a4
ogolubeva <olga602god@gmail.com>
2022-01-20 12:30:34 +0300

    Домашка 5 урока

---
+++ src/main/java/lesson_1_5/Employee.java
@@ -0,0 +1,74 @@
+package lesson_1_5;
+
+public class Employee {
+    private String fio;
+    private String position;
+    private String email;
+    private String phoneNumber;
+    private int salary;
+    private int age;
+
+    public Employee(String fio, String position, String email, String phoneNumber, int salary, int age) {
+        this.fio = fio;
+        this.position = position;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void info() {
+        System.out.println("Employee: " + fio + ", " + position + ", " + email + ", " + phoneNumber + ", " + salary + ", " + age);
+    }
+
+
+
+}
